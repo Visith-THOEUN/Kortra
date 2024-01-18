a5b4b3362e1923798134da245d888bf5c1688940
Visith Thoeun <visiththoeun797@gmail.com>
2024-01-18 15:43:45 +0700

    hello 3

--- resources/views/guest.blade.php
+++ resources/views/guest.blade.php
@@ -9,5 +9,6 @@
 </head>
 <body>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit maiores, molestiae consequatur ab tempora perferendis optio, hic voluptates id quo repellendus illum repudiandae nulla at nisi earum repellat aperiam reprehenderit.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad quis qui eum provident quas officia, porro laboriosam labore debitis architecto facere saepe quo et iusto pariatur nemo asperiores dicta aliquam.</p>
 </body>
 </html>
