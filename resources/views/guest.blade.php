c03ea5722b87c4d1d8533077181c189ec1f2552c
Visith Thoeun <visiththoeun797@gmail.com>
2024-01-18 12:50:59 +0700

    hello 2

---
+++ resources/views/guest.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    @vite( ['resources/css/app.css' ])
+    @vite( ['resources/sass/app.scss'])
+
+
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit maiores, molestiae consequatur ab tempora perferendis optio, hic voluptates id quo repellendus illum repudiandae nulla at nisi earum repellat aperiam reprehenderit.</p>
+</body>
+</html>
