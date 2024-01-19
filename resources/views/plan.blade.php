fb9083126377bde367c8320f8fce20639fd769b8
Visith Thoeun <visiththoeun797@gmail.com>
2024-01-19 16:15:34 +0700

    helloo

--- resources/views/plan.blade.php
+++ resources/views/plan.blade.php
@@ -34,9 +34,14 @@
                 </div>
             </div>
             <div class="col-sm-4 col-xs-12">
-            <div class="plan01">
+                <div class="plan01">
                     <img src="" alt="">
-                    <p>Lorem ipsum dolor sit amet o tenetur cum.</p>
+                    <p>Prices 35$</p>
+                    <p>Guetst 500</p> 
+                    <p>Users 5</p>
+                    <p>Valid 7 days</p>
+                    <p>10% OFF</p>
+                    <p>total price 31.$</p>
                 </div>
             </div>
         </div>
@@ -44,22 +49,38 @@
             <div class="col-sm-4 col-xs-12">
                 <div class="plan01">
                     <img src="" alt="">
-                    <p>Lorem ipsum dolor sit amet o tenetur cum.</p>
+                    <p>Prices 25$</p>
+                    <p>Guetst 300</p> 
+                    <p>Users 5</p>
+                    <p>Valid 7 days</p>
+                    <p>15% OFF</p>
+                    <p>total price 21$</p>
+                    
                 </div>
             </div>
             <div class="col-sm-4 col-xs-12">
                 <div class="plan01">
                     <img src="" alt="">
-                    <p>Lorem ipsum dolor sit amet o tenetur cum.</p>
+                    <p>Prices 35$</p>
+                    <p>Guetst 500</p> 
+                    <p>Users 5</p>
+                    <p>Valid 7 days</p>
+                    <p>10% OFF</p>
+                    <p>total price 31.$</p>
                 </div>
             </div>
             <div class="col-sm-4 col-xs-12">
-            <div class="plan01">
+                <div class="plan01">
                     <img src="" alt="">
-                    <p>Lorem ipsum dolor sit amet o tenetur cum.</p>
+                    <p>Prices 35$</p>
+                    <p>Guetst 500</p> 
+                    <p>Users 5</p>
+                    <p>Valid 7 days</p>
+                    <p>10% OFF</p>
+                    <p>total price 31.$</p>
                 </div>
             </div>
-        </div>
+       </div>
     </div>
 </body>
 </html>
