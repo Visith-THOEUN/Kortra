bd9a3f9994dfa1fc6f941e721110996ebd881671
thoeunsopheara <thoeunsopheara@gmail.com>
2024-03-02 21:46:29 +0700

    add group menu to sidebar

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,12 +1,12 @@
 <!-- need to remove -->
 <li class="nav-item">
-    <a href="{{ route('home') }}" class="nav-link {{ Request::is('home') ? 'active' : '' }}">
+    <a href="{{ route('home') }}" class="nav-link {{ Request::is('admin/home') ? 'active' : '' }}">
         <i class="fas fa-house-user"></i>
         <p>Home</p>
     </a>
 </li>
 <li class="nav-item">
-    <a href="{{ route('event') }}" class="nav-link {{ Request::is('event') ? 'active' : '' }}">
+    <a href="{{ route('event') }}" class="nav-link {{ Request::is('admin/event') ? 'active' : '' }}">
         <i class="fas fa-calendar-week"></i>
         <p>Event</p>
     </a>
@@ -19,21 +19,28 @@
     </a>
 </li> -->
 <li class="nav-item">
-    <a href="{{ route('guest') }}" class="nav-link {{ Request::is('guest') ? 'active' : '' }}">
+    <a href="{{ route('guest') }}" class="nav-link {{ Request::is('admin/guest') ? 'active' : '' }}">
         <i class="fas fa-users"></i>
         <p>Guest</p>
     </a>
 </li>
 <li class="nav-item">
-    <a href="{{ route('write') }}" class="nav-link {{ Request::is('write') ? 'active' : '' }}">
+    <a href="{{ route('write') }}" class="nav-link {{ Request::is('admin/write') ? 'active' : '' }}">
         <i class="fas fa-pen-alt"></i>
         <p>Write</p>
     </a>
 </li>
 <li class="nav-item">
-    <a href="{{ route('user') }}" class="nav-link {{ Request::is('user') ? 'active' : '' }}">
+    <a href="{{ route('user') }}" class="nav-link {{ Request::is('admin/user') ? 'active' : '' }}">
         <i class="fas fa-users"></i>
         <p>User</p>
+    </a>
+</li>
+
+<li class="nav-item">
+    <a href="{{ route('groups.index') }}" class="nav-link {{ Request::is('admin/groups') || Request::is('admin/groups/*') ? 'active' : '' }}">
+    <i class="fas fa-user-friends"></i>
+    <p>Groups</p>
     </a>
 </li>
 
