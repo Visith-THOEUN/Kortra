cc9c0c126eb1535cd9de8d31514d0041f82ed1f8
thoeunsopheara <thoeunsopheara@gmail.com>
2024-03-02 09:06:40 +0700

    diable registeration and cleanup route

--- routes/web.php
+++ routes/web.php
@@ -17,34 +17,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('event');
+Route::prefix("admin")->group(function(){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/event', [App\Http\Controllers\EventController::class, 'index'])->name('event');
+    Route::get('/plan', [App\Http\Controllers\PlanController::class, 'index'])->name('plan');
+    Route::get('/guest', [App\Http\Controllers\GuestController::class, 'index'])->name('guest');
+    Route::get('/wirte', [App\Http\Controllers\WriteController::class, 'index'])->name('write');
+    Route::get('/user', [App\Http\Controllers\UserController::class, 'index'])->name('user');
 });
 
-Route::get('/', function () {
-    return view('plan');
-});
-
-Route::get('/', function () {
-    return view('guest');
-});
-Route::get('/', function () {
-    return view('write');
-});
-Route::get('/', function () {
-    return view('user');
-});
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/event', [App\Http\Controllers\EventController::class, 'index'])->name('event');
-
-Route::get('/plan', [App\Http\Controllers\PlanController::class, 'index'])->name('plan');
-
-Route::get('/guest', [App\Http\Controllers\GuestController::class, 'index'])->name('guest');
-
-Route::get('/wirte', [App\Http\Controllers\WriteController::class, 'index'])->name('write');
-
-Route::get('/user', [App\Http\Controllers\UserController::class, 'index'])->name('user');
-
-Auth::routes();
+Auth::routes(["register" => false]);
