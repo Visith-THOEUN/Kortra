9ed741609761795834ad1648dbdb16a617a59f19
thoeunsopheara <thoeunsopheara@gmail.com>
2024-03-04 16:34:35 +0700

    add event controller and route

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,25 +2,81 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreEventRequest;
+use App\Http\Requests\UpdateEventRequest;
+use App\Models\Event;
+use App\Models\Group;
+
 class EventController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * Display a listing of the resource.
      */
     public function index()
     {
-        return view('event');
+        $events = Event::paginate(10);
+
+        return view('events.index', ['events' => $events]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $groups = Group::all()->pluck('name', 'id')->prepend('Please select group', '');
+
+        return view('events.create', ['groups' => $groups]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEventRequest $request)
+    {
+        Event::create($request->all());
+
+        return redirect()->route('events.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $event = Event::where('id', $id)->first();
+
+        return view('events.show', ['event' => $event]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $event = Event::where('id', $id)->first();
+        $groups = Group::all()->pluck('name', 'id')->prepend('Please select group', '');
+
+        return view('events.edit', ['event' => $event, 'groups' => $groups]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEventRequest $request, string $id)
+    {
+        Event::where('id', $id)->update($request->validated());
+
+        return redirect()->route('events.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Event::where('id', $id)->delete();
+
+        return redirect()->route('events.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\GroupController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -21,10 +22,10 @@ Route::get('/', function () {
 
 Route::middleware('auth')->prefix('admin')->group(function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::get('/event', [App\Http\Controllers\EventController::class, 'index'])->name('event');
     Route::get('/plan', [App\Http\Controllers\PlanController::class, 'index'])->name('plan');
     Route::get('/guest', [App\Http\Controllers\GuestController::class, 'index'])->name('guest');
     Route::get('/wirte', [App\Http\Controllers\WriteController::class, 'index'])->name('write');
+    Route::resource('/events', EventController::class);
     Route::resource('/users', UserController::class);
     Route::resource('groups', GroupController::class);
 })->middleware('auth');
