49101db605aacf9b224a5ef1b367605e89db536f
thoeunsopheara <thoeunsopheara@gmail.com>
2024-03-05 22:00:57 +0700

    remove unused menus

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,10 +1,10 @@
-<!-- need to remove -->
 <li class="nav-item">
     <a href="{{ route('home') }}" class="nav-link {{ Request::is('admin/home') ? 'active' : '' }}">
         <i class="fas fa-house-user"></i>
         <p>Home</p>
     </a>
 </li>
+
 <li class="nav-item">
     <a href="{{ route('events.index') }}"
         class="nav-link {{ Request::is('admin/events') || Request::is('admin/events/*') ? 'active' : '' }}">
@@ -13,24 +13,14 @@
     </a>
 </li>
 
-<!-- <li class="nav-item">
-    <a href="{{ route('plan') }}" class="nav-link {{ Request::is('plan') ? 'active' : '' }}">
-        <i class="fas fa-seedling"></i>
-        <p>Plan</p>
-    </a>
-</li> -->
-<li class="nav-item">
-    <a href="{{ route('guest') }}" class="nav-link {{ Request::is('admin/guest') ? 'active' : '' }}">
-        <i class="fas fa-users"></i>
-        <p>Guest</p>
-    </a>
-</li>
-<li class="nav-item">
-    <a href="{{ route('write') }}" class="nav-link {{ Request::is('admin/write') ? 'active' : '' }}">
-        <i class="fas fa-pen-alt"></i>
-        <p>Write</p>
-    </a>
-</li>
+{{--<li class="nav-item">--}}
+{{--    <a href="{{ route('guests.index') }}"--}}
+{{--        class="nav-link {{ Request::is('admin/guests') || Request::is('admin/guests/*') ? 'active' : '' }}">--}}
+{{--        <i class="fas fa-users"></i>--}}
+{{--        <p>Guest</p>--}}
+{{--    </a>--}}
+{{--</li>--}}
+
 <li class="nav-item">
     <a href="{{ route('users.index') }}"
         class="nav-link {{ Request::is('admin/users') || Request::is('admin/users/*') ? 'active' : '' }}">
