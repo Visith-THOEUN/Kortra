97dbdda3f91ef97a6a8851e4fcc902de26e4653b
thoeunsopheara <thoeunsopheara@gmail.com>
2024-03-27 21:09:11 +0700

    add guests method to event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
@@ -15,5 +16,10 @@ class Event extends Model
     public function group(): BelongsTo
     {
         return $this->BelongsTo(Group::class);
+    }
+
+    public function guests(): HasMany
+    {
+        return $this->HasMany(Guest::class);
     }
 }
