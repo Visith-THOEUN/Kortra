607c8165b31bce341b1faca2b03168bd8f906164
sopheara-kdc <sopheara@kirirom-digital.com>
2024-08-21 22:33:27 +0700

    allow fillable khqr fields

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -11,7 +11,7 @@ class Event extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'detail', 'event_date', 'group_id'];
+    protected $fillable = ['name', 'detail', 'event_date', 'group_id', 'khqr_khr', 'khqr_usd'];
 
     public function group(): BelongsTo
     {
